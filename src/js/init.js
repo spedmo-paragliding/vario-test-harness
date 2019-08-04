d213af7f7f15b617f07e0f8b58673e8c0e036bce
Aaron Hughes <aaron.hughes@spedmo.com>
2019-08-04 17:25:14 +0800

    Basic ble console output logger

--- src/js/init.js
+++ src/js/init.js
@@ -1,5 +1,22 @@
+var varioDisplay, consoleOutput;
+
 $.spedmo = $.spedmo || {};
 $.spedmo.bleReady = function() {
+  varioDisplay = $('#varioDisplay');
+
+  varioDisplay.append('<h1>Vario Display</h1>');
+  varioDisplay.append('<div class="container"><div class="row"><div class="col-xs-12"><div class="consoleOutput"></div></div></div></div>');
+
+  consoleOutput = $('.consoleOutput');
+
+  // define our custom events
+	$.spedmo.ble.event.rawBleLineFeed = function(message) {
+		consoleOutput.html($.spedmo.ble.console);
+		consoleOutput.scrollTop(consoleOutput.prop('scrollHeight'));
+	};
+
+  // start the ble device scan
+  $.spedmo.ble.scan();
+
   console.log('Vario JS initalised')
-  $('#varioDisplay').html('<h1>Vario Display</h1>');
 }
